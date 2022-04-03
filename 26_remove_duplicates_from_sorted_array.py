80076f44b950f01deef95a1320137c0a534aee47
Rosenquartz <tayangelera@gmail.com>
2022-04-03 19:45:03 +0800

    Maximum subarray

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -1,46 +1,21 @@
 class Solution(object):
-    def maxSubArray(self, nums):
+    def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1:
-            return nums[0]
-        if max(nums) < 0:
-            return max(nums)
-        sums = []
-        sum = 0
-        for i in range(len(nums)):
-            sum += nums[i]
-            sums.append(sum)
-        print(sums)
-
-        currMin = sums[0]
-        currMax = 0
-        validMax = 0
-        currAns = 0
-        validAns = 0
-
-        for i in range(1, len(sums)):
-            if sums[i]<currMin:
-                currMin = sums[i]
-                validMax = 0
-            elif validMax == 0:
-                currMax = sums[i]
-                validMax = 1
-                if validAns and (currMax-currMin)>currAns:
-                    currAns = currMax - currMin
-                elif validAns == 0:
-                    currAns = currMax - currMin
-                    validAns = 1
-            elif sums[i]>currMax:
-                currMax = sums[i]
-                if validAns and (currMax-currMin)>currAns:
-                    currAns = currMax - currMin
-                elif validAns == 0:
-                    currAns = currMax - currMin
-                    validAns = 1
-
-        if validAns:
-            sums.append(currAns)
-        return (max(sums))
+        saveCounter = 0
+        moveCounter = 1
+        if len(nums)==0:
+            return 0
+        while(1):
+            if moveCounter >= len(nums):
+                break
+            if nums[moveCounter] == nums[saveCounter]:
+                moveCounter += 1
+            else:
+                saveCounter += 1
+                nums[saveCounter]=nums[moveCounter]
+                moveCounter += 1
+        return saveCounter + 1
+        
