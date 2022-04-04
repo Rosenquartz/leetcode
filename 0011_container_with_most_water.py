0a7c06505ee07cdb88464dd73e3289d02e3a13f3
Rosenquartz <tayangelera@gmail.com>
2022-04-04 09:47:23 +0800

    Rename files

--- 0011_container_with_most_water.py
+++ 0011_container_with_most_water.py
@@ -8,7 +8,7 @@ class Solution(object):
         a = 0
         b = n - 1
         current_max = min(height[a],height[b]) * (n-1)
-        # Idea: reduce width of container by at least one per iteration.
+        # Idea: reduce width of container by at least one per iteration
         while (a < b):
             new_area_one = min(height[a], height[b-1]) * (b-a-1)
             new_area_two = min(height[a+1], height[b]) * (b-a-1)
@@ -20,6 +20,7 @@ class Solution(object):
                     b = b - 1
                 else:
                     a = a + 1
+            # Else just update a and b
             else:
                 if height[a] > height[b]:
                     b = b - 1
