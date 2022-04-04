ff02ee7a714d680f64178b8d0bcb46308f0bd69f
Rosenquartz <tayangelera@gmail.com>
2022-04-04 10:14:39 +0800

    First iteration: too slow

--- 0015_practice.py
+++ 0015_practice.py
@@ -8,11 +8,23 @@ def threeSum(nums):
 		else:
 			indices[nums[i]] = [i]
 
+	lists = []
 	for i in range(len(nums)):
 		for j in range(i+1, len(nums)):
 			third_value = -1 * (nums[i] + nums[j])
-			print(i,j,indices[third_value])
-	pass
+			if third_value in indices:
+				if max(indices[third_value]) > j:
+					#print(i,nums[i],j,nums[j],third_value)
+					new_list = [nums[i], nums[j], third_value]
+					new_list.sort()
+					if new_list in lists:
+						continue
+					else:
+						lists.append(new_list)
+	return lists
 
 print(threeSum([]))
 print(threeSum([-1,0,1,2,-1,-4]))
+
+# First iteration: too slow
+# Duplicate values can be preprocessed by making a dictionary instead
